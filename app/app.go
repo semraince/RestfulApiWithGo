a0f8816c5e93fcf0343179dc0ea16025cb15000f
semraince <semrance9@gmail.com>
2021-11-13 12:25:46 +0300

    app init address changed

--- app/app.go
+++ app/app.go
@@ -9,44 +9,6 @@ import (
 	"os"
 )
 
-/*var routes = []route{
-	newRoute("GET", "/", home),
-	newRoute("GET", "/contact", contact),
-	newRoute("GET", "/api/widgets", apiGetWidgets)
-}
-
-func newRoute(method, pattern string, handler http.HandlerFunc) route {
-	return route{method, regexp.MustCompile("^" + pattern + "$"), handler}
-}
-
-type route struct {
-	method  string
-	regex   *regexp.Regexp
-	handler http.HandlerFunc
-}
-
-func Serve(w http.ResponseWriter, r *http.Request) {
-	var allow []string
-	for _, route := range routes {
-		matches := route.regex.FindStringSubmatch(r.URL.Path)
-		if len(matches) > 0 {
-			if r.Method != route.method {
-				allow = append(allow, route.method)
-				continue
-			}
-			ctx := context.WithValue(r.Context(), ctxKey{}, matches[1:])
-			route.handler(w, r.WithContext(ctx))
-			return
-		}
-	}
-	if len(allow) > 0 {
-		w.Header().Set("Allow", strings.Join(allow, ", "))
-		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.NotFound(w, r)
-}*/
-
 func Init() {
 	addr := config.Get().Servers.AppServer
 	envPort := os.Getenv("PORT")
